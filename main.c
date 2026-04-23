b82cdf97f125fee2095d986cf5876a2618e80497
NinoM-GameDev <montanaronino04@gmail.com>
2026-04-23 19:11:04 -0300

    ventana basica

--- main.c
+++ main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "GBT/gbt.h"
+
 int main()
 {
-    printf("Hello world!\n");
+
+    gbt_iniciar();
+
+    gbt_crear_ventana("TETRIS!", 256,150, 4);
+
+    int corriendo = 1;
+
+    while (corriendo)
+    {
+        gbt_procesar_entrada();
+        eGBT_Tecla tecla = gbt_obtener_tecla_presionada();
+        if (tecla == GBTK_ESCAPE)
+            corriendo = 0;
+        gbt_esperar(16);
+    }
+
+    gbt_cerrar();
+
     return 0;
 }
